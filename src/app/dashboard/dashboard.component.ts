a947d4ec3b8a4ef2d4649064d46a711b58d756a9
Johnny Perdomo <johnnyperdomo7@gmail.com>
2022-12-26 17:35:34 -0500

    query all collections in dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,66 +12,53 @@ export class DashboardComponent implements OnInit {
   numberOfTextReviews: number = 0;
   numberOfVideoReviews: number = 0;
 
-  collections: any[];
+  collections: any[] = []
 
   constructor(private db: AngularFirestore, private auth: AngularFireAuth) {
-    this.collections = [
-      {
-        id: 1,
-        name: 'Coffee Shop',
-        numberOfReviews: 4,
-        img: 'https://picsum.photos/200',
-      },
-      {
-        id: 2,
-        name: 'Bar Store',
-        numberOfReviews: 7,
-        img: 'https://picsum.photos/200',
-      },
-      {
-        id: 2,
-        name: 'Restaurant',
-        numberOfReviews: 7,
-        img: 'https://picsum.photos/200',
-      },
-      {
-        id: 2,
-        name: 'car wash',
-        numberOfReviews: 7,
-        img: 'https://picsum.photos/200',
-      },
-    ];
+  
   }
 
   ngOnInit(): void {
-    this.getReviewStats();
-    this.getAllCollections();
+    this.auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        await this.getReviewStats(user.uid);
+        await this.getAllCollections(user.uid);
+      }
+    });
   }
 
-  async getAllCollections() {
+  async getAllCollections(userID: string) {
     //TODO: query all collections connected to userID
+    try {
+      this.db
+        .collection('collections', (ref) => ref.where('userID', '==', userID))
+        .valueChanges()
+        .subscribe((data) => {
+          console.log(data);
+
+          this.collections = data;
+        });
+    } catch (error) {
+      alert(error);
+      //LATER: do something here
+    }
   }
 
-  async getReviewStats() {
+  async getReviewStats(userID: string) {
     try {
-      this.auth.onAuthStateChanged(async (user) => {
-        if (user) {
-          this.db
-            .collection('users')
-            .doc(user.uid)
-            .valueChanges()
-            .subscribe((data: any) => {
-              if (data) {
-                let stats = data.stats;
-                this.allReviews =
-                  stats.no_text_reviews + stats.no_video_reviews;
-              } else {
-                throw Error;
-                //LATER: update this to just show a 404 if anything
-              }
-            });
-        }
-      });
+      this.db
+        .collection('users')
+        .doc(userID)
+        .valueChanges()
+        .subscribe((data: any) => {
+          if (data) {
+            let stats = data.stats;
+            this.allReviews = stats.no_text_reviews + stats.no_video_reviews;
+          } else {
+            throw Error;
+            //LATER: update this to just show a 404 if anything
+          }
+        });
     } catch (error) {
       alert(error);
       //LATER: do something here
