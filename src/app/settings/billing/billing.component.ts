591bf5c4a3ae3cd149545ccf7ea0bb6f2f60109b
Johnny Perdomo <johnnyperdomo7@gmail.com>
2023-01-02 16:42:11 -0500

    add custom portal link depending on environment mode

--- src/app/settings/billing/billing.component.ts
+++ src/app/settings/billing/billing.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, isDevMode } from '@angular/core';
 
 @Component({
   selector: 'app-billing',
   templateUrl: './billing.component.html',
-  styleUrls: ['./billing.component.scss']
+  styleUrls: ['./billing.component.scss'],
 })
-export class BillingComponent implements OnInit {
+export class BillingComponent {
+  customerPortalLink: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    if (isDevMode()) {
+      //development: test link for customer portal
+      this.customerPortalLink =
+        'https://billing.stripe.com/p/login/test_5kAdTN9cK3lT0QU144';
+    } else {
+      //production: live link for customer portal
+      this.customerPortalLink =
+        'https://billing.stripe.com/p/login/aEUaGO1KlaPlfbWeUU';
+      console.log('in production');
+    }
   }
-
 }
