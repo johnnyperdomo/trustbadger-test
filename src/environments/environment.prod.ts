087cd0f20d1cef0faac378339a41a0eac1377aab
Johnny Perdomo <johnnyperdomo7@gmail.com>
2023-01-07 12:59:15 -0500

    fix firebase config files for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,16 @@
 export const environment = {
   firebase: {
-    projectId: 'trustbadger-dev',
-    appId: '1:646066223104:web:1ada481ac07dbdeb7540e5',
-    storageBucket: 'trustbadger-dev.appspot.com',
-    apiKey: 'AIzaSyAcUh_xFxY4sx9uAWP5vbpluuUrP_GZ1nA',
-    authDomain: 'trustbadger-dev.firebaseapp.com',
-    messagingSenderId: '646066223104',
+    projectId: 'trustbadger-prod',
+    apiKey: 'AIzaSyCqd5ne7Zkvor-UZGCDS8XuAL5Keqf5smM',
+    authDomain: 'trustbadger-prod.firebaseapp.com',
+    storageBucket: 'trustbadger-prod.appspot.com',
+    messagingSenderId: '608421562834',
+    appId: '1:608421562834:web:9a40749e008a67e70b200b',
   },
   production: true,
   stripe: {
     starterPriceID: 'price_1MLZJtBbTwqiwTLE4h0jIhVe',
-    publishableKey: 'pk_live_51MLUx0BbTwqiwTLERcqEaGlgtoOcqPehvrpQpadO5EgI2T0XbR89AnZ1TwRe6m81YzfVtEKQeLaNKFm5IoBwEoN700vVvAeQg4',
+    publishableKey:
+      'pk_live_51MLUx0BbTwqiwTLERcqEaGlgtoOcqPehvrpQpadO5EgI2T0XbR89AnZ1TwRe6m81YzfVtEKQeLaNKFm5IoBwEoN700vVvAeQg4',
   },
 };
