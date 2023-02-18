57db3c0275882868816e40bf33b35d9e4064957a
Johnny Perdomo <johnnyperdomo7@gmail.com>
2023-02-18 18:12:44 -0500

    git status

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,19 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
-  constructor(private auth: AngularFireAuth, private router: Router) { }
+  //FIX: collection navbar and normal navbar dropdown not working in mobile view
+  constructor(private auth: AngularFireAuth, private router: Router) {}
 
   async onLogout() {
     await this.auth.signOut();
     this.router.navigate(['/login']);
   }
-
 }
